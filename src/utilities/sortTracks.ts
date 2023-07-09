49f18a96fe7de2db15c7d293d14fbca2b8035462
Solest <zkordo044@gmail.com>
2023-07-08 22:17:51 -0400

    Removed vestigial files.

--- src/utilities/sortTracks.ts
+++
@@ -1,9 +0,0 @@
-import { getAudioFeatures, getTrack } from "../fixtures/data";
-import { Track, TrackID } from "../types";
-import { useCache } from "./useCache";
-
-export function sortTracks(comboTracks, sortParameter): TrackID[] {
-    let sortedTracks: TrackID[] = [];
-
-    return sortedTracks;
-}
